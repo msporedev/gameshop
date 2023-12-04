d204b134afa6d1630bbf56e88c70eac52d67634d
msporedev <gamextra85@gmail.com>
2023-12-04 16:19:21 +0330

    i edit javascript

--- data/login.js
+++ data/login.js
@@ -1,3 +1,13 @@
+let captchaText = document.getElementById('captcha');
+var ctx = captchaText.getContext("2d");
+ctx.font = "30px Roboto";
+ctx.fillStyle = "#08e5ff";
+
+let userText = document.getElementById('textBox');
+let submitButton = document.getElementById('submitButton');
+let output = document.getElementById('output');
+let refreshButton = document.getElementById('refreshButton');
+
 var captchaStr = "";
 
 let alphaNums = ['A', 'B', 'C', 'D', 'E', 'F', 'G',
