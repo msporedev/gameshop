24352d9e224090d88ba6339ca528697cba26ed26
msporedev <gamextra85@gmail.com>
2023-12-16 22:53:02 +0330

    i add js on gameshop page

--- js/gameshop.js
+++ js/gameshop.js
@@ -1,1 +1,6 @@
-let html = document.querySelector('btn').ELEMENT_NODE;
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+// });
+var signup = document.getElementById("signup-btn");
+signup.setAttribute('href', './profile.html');
+signup.children[1].innerHTML = 'Profile';
