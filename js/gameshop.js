f77de503f10acd89f184276989dab97668430beb
msporedev <gamextra85@gmail.com>
2023-12-20 14:30:26 +0330

    i work on account button

--- js/gameshop.js
+++ js/gameshop.js
@@ -1,6 +1,12 @@
 
 // document.addEventListener("DOMContentLoaded", function(event) {
 // });
-var signup = document.getElementById("signup-btn");
-signup.setAttribute('href', './profile.html');
-signup.children[1].innerHTML = 'Profile';
+// var signup = document.getElementById("signup-btn");
+// signup.setAttribute('href', './profile.html');
+// signup.children[1].innerHTML = 'Profile';
+
+// var signup = document.getElementById("signup-btn");
+// let login = true;
+// let not_login = false;
+// console.log(login);
+// console.log(not_login);
